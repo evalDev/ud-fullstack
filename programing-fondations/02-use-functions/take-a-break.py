0842f2e595bc66a70f995644fc6488c618646a55
@evalDev <WhatAGit@developingdan.com>
2016-07-26 00:54:00 +0100

    added current time to break message

--- programing-fondations/02-use-functions/take-a-break.py
+++ programing-fondations/02-use-functions/take-a-break.py
@@ -4,5 +4,6 @@ import webbrowser
 breaks = 3
 for iter in range(breaks):
     time.sleep(10)
-    print "This is break number", iter + 1, "of", breaks 
-   # webbrowser.open("https://youtu.be/UmO9LmvLVyM")
+    print "Time to take a break it's", time.ctime()
+    print "This is break number", iter + 1, "of", breaks,"\n" 
+    webbrowser.open("https://youtu.be/UmO9LmvLVyM")
