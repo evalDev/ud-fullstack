37b8647437a9226cae1ef6e916e3abe0d1f4c4ee
@evalDev <WhatAGit@developingdan.com>
2016-07-26 08:50:35 +0100

    secret message script renames files

--- programing-fondations/02-use-functions/secret-message.py
+++ programing-fondations/02-use-functions/secret-message.py
@@ -1,8 +1,14 @@
 import os
 def rename_files():
+    files_dir = "/home/mute/repos/udacity/programing-fondations//02-use-functions/prank"
+    saved_path = os.getcwd()
+    os.chdir(files_dir) 
     #1. Get the name of each file in folder
-    file_list = os.listdir("/home/mute/repos/udacity/programing-fondations//02-use-functions/prank")
-    print file_list
-#2. remove numbers from each file name
+    file_list = os.listdir(files_dir)
+    for file_name in file_list:
+        #2. remove numbers from each file name
+        file_name_no_numbers = file_name.translate(None, "0123456789")
+        os.rename(file_name, file_name_no_numbers)
 
+    os.chdir(saved_path)
 rename_files()
