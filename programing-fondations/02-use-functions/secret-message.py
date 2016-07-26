5a8e96d24b0334e7241c6de393e8afa71463499d
@evalDev <WhatAGit@developingdan.com>
2016-07-26 08:57:50 +0100

    secret message script added user info

--- programing-fondations/02-use-functions/secret-message.py
+++ programing-fondations/02-use-functions/secret-message.py
@@ -8,6 +8,7 @@ def rename_files():
     for file_name in file_list:
         #2. remove numbers from each file name
         file_name_no_numbers = file_name.translate(None, "0123456789")
+        print "Changing:", file_name, "to:", file_name_no_numbers
         os.rename(file_name, file_name_no_numbers)
 
     os.chdir(saved_path)
